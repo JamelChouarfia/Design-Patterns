b24439079147181cacfa3d9fc4dfd8c262c564c3
Ultragum <89540611+Ultragum@users.noreply.github.com>
2023-01-30 00:01:02 -0500

    Updated Flavor Comments

--- decorator/Chocolate.java
+++ decorator/Chocolate.java
@@ -1,6 +1,11 @@
 package decorator;
 
 public class Chocolate extends ScoopDecorator {
+    /**
+     * Adds Scoops to the Ice Cream, which is iceCream
+     * @param iceCream The Ice Cream that the scoops are being added to
+     * @param numScoops The amount of scoops
+     */
     public Chocolate(IceCream iceCream, int numScoops) {
         super(iceCream, numScoops);
     }
--- decorator/Vanilla.java
+++ decorator/Vanilla.java
@@ -1,6 +1,11 @@
 package decorator;
 
 public class Vanilla extends ScoopDecorator {
+    /**
+     * Adds Scoops to the Ice Cream, which is iceCream
+     * @param iceCream The Ice Cream that the scoops are being added to
+     * @param numScoops The amount of scoops
+     */
     public Vanilla(IceCream iceCream, int numScoops) {
         super(iceCream, numScoops);
     }
